a45efbe74ca9da1472c68208bb9f7699b73e9431
Milena <167583741+mihnobrega@users.noreply.github.com>
2026-09-07 23:23:04 -0300

    finaliza configuracao PWA

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
       registerType: "autoUpdate",
 
       includeAssets: [
-        "favicon.ico",
+        "favicon.svg",
         "apple-touch-icon.png",
         "pwa-192x192.png",
         "pwa-512x512.png"
